228e9728c6df39d1c2b795cca919699cb8a1e5a2
rufinomendoza <ThinkPadT400@Mobile>
2012-08-18 15:36:03 -0400

    Changed root in routes to holdings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to holdings_url, :notice => "Signed up!"
     else
       render "new"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ButtonwoodTwo::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => 'sessions#new'
+  root :to => 'holdings#index'
   resources :users
   resources :sessions
   
