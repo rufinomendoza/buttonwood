69e896fe4edc552ec5b8996947ff2c9c76d2ad5e
rufinomendoza <ThinkPadT400@Mobile>
2012-08-30 18:31:54 -0400

    Change in domain from localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,13 +15,13 @@ ButtonwoodTwo::Application.configure do
 
   # Don't care if the mailer can't send
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "stormy-forest-4818.herokuapp.com" }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
+    :domain               => 'stormy-forest-4818.herokuapp.com',
     :user_name            => 'rufino.a.mendoza@gmail.com',
     :password             => 'zbntyswcxbxphjlu',
     :authentication       => 'plain',
