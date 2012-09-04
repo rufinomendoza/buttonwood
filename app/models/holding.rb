2e35cc0fdb867624828d4d996f5f621a26402ec7
rufinomendoza <ThinkPadT400@Mobile>
2012-09-04 01:42:40 -0400

    Changed API breaker to while parsed.blank? do. Works better, but nil error still raised.

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -127,7 +127,7 @@ class Holding < ActiveRecord::Base
 
   def retrieve_from_yahoo
     parsed = {}
-    while parsed.blank?
+    while parsed.blank? do
       url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22#{symbol}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
       resp = Net::HTTP.get_response(URI.parse(url))
       body = JSON.parse(resp.body)
