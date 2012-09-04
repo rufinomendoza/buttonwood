a185dab4658cb661a920b8a44be94a7e26d1d042
rufinomendoza <ThinkPadT400@Mobile>
2012-09-03 23:24:36 -0400

    Fixed parsed error in retrieve_from_yahoo

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -126,6 +126,7 @@ class Holding < ActiveRecord::Base
   private
 
   def retrieve_from_yahoo
+    parsed = {}
     while parsed.blank?
       url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22#{symbol}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
       resp = Net::HTTP.get_response(URI.parse(url))
