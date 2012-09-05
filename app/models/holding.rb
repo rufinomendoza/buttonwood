5ad6144a087a2b14037d2911f0e0e1283fc5d4a2
Rufino Mendoza <iMac_2009@Home>
2012-09-04 23:48:05 -0400

    Removed caching from retrieve_from_yahoo as it increased process times

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -132,7 +132,7 @@ class Holding < ActiveRecord::Base
       resp = Net::HTTP.get_response(URI.parse(url))
       body = JSON.parse(resp.body)
       pretty = JSON.pretty_generate(body)
-      parsed = Rails.cache.fetch("#{symbol}") {JSON.parse(pretty)["query"]["results"]}
+      parsed = JSON.parse(pretty)["query"]["results"]
     end
     result = parsed["quote"]
   end
