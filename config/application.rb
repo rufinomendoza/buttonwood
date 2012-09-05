188e0f857a7f3d663323f0c7a74e85cd081dda1f
Rufino Mendoza <iMac_2009@Home>
2012-09-05 19:09:24 -0400

    Added precompile initialization change in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module ButtonwoodTwo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added this to try and fix pw reset in production.
+    config.assets.initialize_on_precompile = false
   end
 end
